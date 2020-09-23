1a955efb4551d34a7467b02c79be8910a488cd1b
Eric Gaupp <eric.gaupp@gmail.com>
2020-09-23 16:58:51 -0400

    GraphQL working with separated schemas, resolvers, typescript types and front-end client with routing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -8,47 +8,95 @@ const helloWorld = gql`
   }
 `;
 
-const danger = gql`
-  query danger {
-    danger
+const authorsQuery = gql`
+  query {
+    authors {
+      id
+      name
+      publications
+    }
   }
 `;
 
-const DangerRoute: React.FC = () => {
-  const { loading, error, data } = useQuery(danger);
-
-  if (loading) return <h4>Loading</h4>;
-  if (error) {
-    console.log(error);
-    return <h4>Error</h4>;
+const publicationsQuery = gql`
+  query {
+    publicationSort {
+      id
+      name
+      publications
+    }
   }
-
-  return <h4>{data["imindanger"]}</h4>;
-};
+`;
 
 const HelloRoute: React.FC = () => {
   const { loading, error, data } = useQuery(helloWorld);
 
   if (loading) return <h4>Loading</h4>;
   if (error) {
-    console.log(error);
+    console.error(error);
     return <h4>Error</h4>;
   }
 
   return <h4>{data["helloWorld"]}</h4>;
 };
 
+const AuthorsRoute: React.FC = () => {
+  const { loading, error, data } = useQuery(authorsQuery);
+
+  if (loading) return <h4>Loading</h4>;
+  if (error) {
+    console.error(error);
+    return <h4>Error</h4>;
+  }
+
+  return data.authors.map(
+    (author: { id: string; name: string; publications: number }) => {
+      return (
+        <div key={author.id}>
+          <h3>{author.name}</h3>
+          <h5>{`Publications: ${author.publications}`}</h5>
+        </div>
+      );
+    }
+  );
+};
+
+const PublicationsRoute: React.FC = () => {
+  const { loading, error, data } = useQuery(publicationsQuery);
+
+  if (loading) return <h4>Loading</h4>;
+  if (error) {
+    console.error(error);
+    return <h4>Error</h4>;
+  }
+
+  return data.publicationSort.map(
+    (author: { id: string; name: string; publications: number }) => {
+      return (
+        <div key={author.id}>
+          <h3>{author.name}</h3>
+          <h5>{`Publications: ${author.publications}`}</h5>
+        </div>
+      );
+    }
+  );
+};
+
 const App: React.FC = () => {
   return (
     <>
-      <Link to="/danger">Danger</Link>
       <Link to="/hello">Hello</Link>
+      <Link to="/authors">Authors</Link>
+      <Link to="/publications">Publications</Link>
       <Switch>
-        <Route exact path="/danger">
-          <DangerRoute />
-        </Route>
         <Route exact path="/hello">
           <HelloRoute />
+        </Route>
+        <Route exact path="/authors">
+          <AuthorsRoute />
+        </Route>
+        <Route exact path="/publications">
+          <PublicationsRoute />
         </Route>
       </Switch>
     </>
