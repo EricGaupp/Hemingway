eeb29fb9655b4263380c32db719c36062402333e
Eric Gaupp <eric.gaupp@gmail.com>
2020-09-29 22:57:47 -0400

    Set application env variables for rebuild

--- app/src/components/Login.tsx
+++ app/src/components/Login.tsx
@@ -4,27 +4,27 @@ const Login = () => {
   return (
     <div className="container mx-auto flex flex-col">
       <a
-        href={`/.auth/login/aad?post_login_redirect_uri=${process.env.REACT_APP_APP_BASE_URI}/dash`}
+        href={`/.auth/login/aad?post_login_redirect_uri=${process.env.REACT_APP_BASE_URI}/dash`}
       >
         Microsoft
       </a>
       <a
-        href={`/.auth/login/facebook?post_login_redirect_uri=${process.env.REACT_APP_APP_BASE_URI}/dash`}
+        href={`/.auth/login/facebook?post_login_redirect_uri=${process.env.REACT_APP_BASE_URI}/dash`}
       >
         Facebook
       </a>
       <a
-        href={`/.auth/login/github?post_login_redirect_uri=${process.env.REACT_APP_APP_BASE_URI}/dash`}
+        href={`/.auth/login/github?post_login_redirect_uri=${process.env.REACT_APP_BASE_URI}/dash`}
       >
         GitHub
       </a>
       <a
-        href={`/.auth/login/google?post_login_redirect_uri=${process.env.REACT_APP_APP_BASE_URI}/dash`}
+        href={`/.auth/login/google?post_login_redirect_uri=${process.env.REACT_APP_BASE_URI}/dash`}
       >
         Google
       </a>
       <a
-        href={`/.auth/login/twitter?post_login_redirect_uri=${process.env.REACT_APP_APP_BASE_URI}/dash`}
+        href={`/.auth/login/twitter?post_login_redirect_uri=${process.env.REACT_APP_BASE_URI}/dash`}
       >
         Twitter
       </a>
